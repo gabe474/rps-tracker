2ceb165f9463274f616bebe026fd9820fe9fe9a3
gabe474 <122543660+gabe474@users.noreply.github.com>
2023-01-31 12:58:38 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,45 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . . # . .
+        . # . # .
+        # . . . #
+        # # # # #
+        # . . . #
+        `)
+    Rounds += 1
+    P1 += 1
+    scores()
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showLeds(`
+        # # # # #
+        . . # . .
+        . . # . .
+        . . # . .
+        . . # . .
+        `)
+    Ties += 1
+    Rounds += 1
+    scores()
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        # # # . .
+        # . . # .
+        # # # . .
+        # . . # .
+        # # # # .
+        `)
+    P2 += 1
+    Rounds += 1
+    scores()
+    basic.clearScreen()
+})
+input.onGesture(Gesture.Shake, function () {
+    reset()
+})
 function scores () {
     OLED.clear()
     OLED.writeStringNewLine("player 1: " + P1)
@@ -8,15 +50,18 @@ function scores () {
     OLED.newLine()
     OLED.writeStringNewLine("Rounds: " + Rounds)
 }
-let Rounds = 0
-let Ties = 0
+function reset () {
+    OLED.init(128, 64)
+    OLED.writeStringNewLine("would you like to play a game?")
+    P1 = 0
+    P2 = 0
+    Ties = 0
+    Rounds = 0
+    basic.pause(2000)
+    scores()
+}
 let P2 = 0
+let Ties = 0
 let P1 = 0
-OLED.init(128, 64)
-OLED.writeStringNewLine("would you like to play a game?")
-P1 = 0
-P2 = 0
-Ties = 0
-Rounds = 0
-basic.pause(2000)
-scores()
+let Rounds = 0
+reset()
